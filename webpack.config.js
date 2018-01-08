1b3e029a5a83ec9edc2454ceb50ef9927f50218d
Wim Olivier <wimo@webtechnique.co.za>
2018-01-08 15:23:43 +0200

    fix typo

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-process.env.NODE_ENV = process.end.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 if (process.env.NODE_ENV === 'test') {                // Jest testing env
   require(dotenv).config({ path: '.env.test' });
