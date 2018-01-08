23980d1d33b4fb0990b680b6f98fbb3e16ebaf43
Wim Olivier <wimo@webtechnique.co.za>
2018-01-08 15:27:11 +0200

    fixed another typo

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,9 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 if (process.env.NODE_ENV === 'test') {                // Jest testing env
-  require(dotenv).config({ path: '.env.test' });
+  require('dotenv').config({ path: '.env.test' });
 } else if (process.env.NODE_ENV === 'development') {  // Normal dev
-  require(dotenv).config({ path: '.env.development' });
+  require('dotenv').config({ path: '.env.development' });
 } // else it's 'production' for Heroku (by default)
 
 module.exports = (env) => {
